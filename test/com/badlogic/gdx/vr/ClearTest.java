3b11ca531f5fafe64efd23002f754002e7528708
badlogic <badlogicgames@gmail.com>
2017-01-29 00:25:02 +0100

    Fixed up ClearTest

--- test/com/badlogic/gdx/vr/ClearTest.java
+++ test/com/badlogic/gdx/vr/ClearTest.java
@@ -1,8 +1,11 @@
 package com.badlogic.gdx.vr;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.vr.VRContext.Eye;
 
 public class ClearTest extends ApplicationAdapter {
 	
@@ -15,9 +18,18 @@ public class ClearTest extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		int glError = Gdx.gl.glGetError();
 		context.pollEvents();
 		context.begin();
-		context.end();
+		context.beginEye(Eye.Left);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		context.endEye();
+		context.beginEye(Eye.Right);
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		context.endEye();
+		context.end();		
 	}
 
 	@Override
